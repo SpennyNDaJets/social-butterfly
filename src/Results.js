7bd52c043ac26928de056778c0b0b1b3dfc55ec4
Spencer <sta7ey@virginia.edu>
2017-06-01 14:26:03 -0400

    Meetup Data  Rendered

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Categories from './data/categories';
 import axios from 'axios';
-
+import Map from './Map';
 let key = '18127e6f127c14484e37b5f5b266679';
 
 class Results extends Component {
@@ -11,21 +11,26 @@ class Results extends Component {
     this.state = {
       zip: this.props.match.params.zip,
       radius: this.props.match.params.radius,
-      category: this.props.match.params.category,  
+      category: this.props.match.params.category,
       eventList: []
     }
 
     this._saveQuery = this._saveQuery.bind(this);
+    this.addToCalendar = this.addToCalendar.bind(this);
   }
 
   _saveQuery = response => {
+    // filter out events without time
+    let newEventList = response.data.filter(event => {
+      return (event.next_event != null)
+    });
+
     const newState = {
       ...this.state,
-      eventList: response.results
+      eventList: newEventList
     };
-    this.setState(newState);
 
-    console.log(this.state);
+    this.setState(newState);
   };
 
   componentWillMount() {
@@ -43,14 +48,61 @@ class Results extends Component {
       url += "&category=" + this.state.category;
     }
     const urlWithKey = url + "&key=" + key;
+    console.log(urlWithKey);
     // make axious request
     axios.get(urlWithKey).then(this._saveQuery);
   }
 
+  addToCalendar(index) {
+    console.log(this.state.eventList[index].next_event.time);
+  }
+
   render() {
+
     return (
       <div className="App">
-        <h1>Results</h1>
+        <div className="App-body">
+          <div className="-list__container">
+            <div className="place-list__header">
+              Events
+            </div>
+            <ul className="place-list">
+              {this.state.eventList.map((event, index) => {
+                return (
+                  <li key={event.id} className="place-list__item">
+                    <div>
+                      <div className="place-list__info">
+                        {event.key_photo != null &&
+                          <img className="thumbImg"
+                            src={event.key_photo.thumb_link}
+                            alt={event.name + "Image"} />
+                        }
+                        <div>
+                          <div className="place-list__item-name">
+                            {event.next_event.name}
+                          </div>
+                          <div className="place-list__item-time">
+                            {Date(event.next_event.time)}
+                          </div>
+                          <div className="place-list__category">
+                            Catagory: {event.category.name}
+                          </div>
+                        </div>
+                      </div>
+                      <button className='button'
+                        onClick={(e) => this.addToCalendar(index)}>
+                        Add to Google Calendar
+                      </button>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          {this.state.eventList.length != 0 &&
+            <Map events={this.state.eventList} lat={this.state.eventList[0].lat} lon={this.state.eventList[0].lon} />
+          }
+        </div>
       </div>
     );
   }
